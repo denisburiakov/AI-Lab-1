223705a8d72b889548f2ce7089d4e3778af14eac
Lab2 <burakovdenis36@gmail.com>
2026-04-18 13:15:41 +0300

    Fiches

--- lab1.py
+++ lab1.py
@@ -1,10 +1,11 @@
-#Lab1--------------------------------------------------------
+# Lab1--------------------------------------------------------
 
-#1-----------------------------------------------------------
+# 1-----------------------------------------------------------
 import pandas as pd
 import matplotlib.pyplot as plt
+
 heart_df = pd.read_csv('heart.csv')
-#2-----------------------------------------------------------
+# 2-----------------------------------------------------------
 df = heart_df.copy()
 print("-" * 100)
 print("First 5 columns: ")
@@ -20,12 +21,12 @@ print("-" * 100)
 print(df.describe())
 print("-" * 100)
 
-#3-----------------------------------------------------------
+# 3-----------------------------------------------------------
 Nan_matrix = df.isnull()
 print(Nan_matrix.sum())
 print("-" * 100)
 
-#4-----------------------------------------------------------
+# 4-----------------------------------------------------------
 df['age'] = df['age'].fillna(df['age'].median())
 df['trestbps'] = df['trestbps'].fillna(df['trestbps'].median())
 df['chol'] = df['chol'].fillna(df['chol'].median())
@@ -33,36 +34,40 @@ df['chol'] = df['chol'].fillna(df['chol'].median())
 print(df.isnull().sum())
 print("-" * 100)
 
-#5-----------------------------------------------------------
+# 5-----------------------------------------------------------
 from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-df[['age', 'trestbps', 'chol', 'thalach', 'oldpeak']] = scaler.fit_transform(df[['age', 'trestbps', 'chol', 'thalach', 'oldpeak']])
 
-#6-----------------------------------------------------------
+scaler = MinMaxScaler()
+df[['age', 'trestbps', 'chol', 'thalach', 'oldpeak']] = scaler.fit_transform(
+    df[['age', 'trestbps', 'chol', 'thalach', 'oldpeak']])
+
+# 6-----------------------------------------------------------
 df = pd.get_dummies(df, columns=['cp', 'thal', 'slope'], drop_first=True)
 
-#Final
+# Final
 print("Final dataset:")
 print(df.head())
 print("-" * 100)
 
-#Lab2--------------------------------------------------------
+# Lab2--------------------------------------------------------
 
-#1-----------------------------------------------------------
+# 1-----------------------------------------------------------
 from sklearn.model_selection import train_test_split
+
 X = df.drop(['thalach', 'target'], axis=1)
 Y = df['thalach']
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
 X_test, X_val, Y_test, Y_val = train_test_split(X_test, Y_test, test_size=0.4, random_state=42)
 
-#2-----------------------------------------------------------
+# 2-----------------------------------------------------------
 from sklearn.linear_model import LinearRegression
+
 linear_model = LinearRegression()
 linear_model.fit(X_train, Y_train)
 Y_pred_test = linear_model.predict(X_test)
 
-#3-----------------------------------------------------------
+# 3-----------------------------------------------------------
 from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_error
 
 MSE = mean_squared_error(Y_test, Y_pred_test)
@@ -74,7 +79,7 @@ print(f"RMSE = {RMSE}")
 print(f"MAE = {MAE}")
 print("-" * 100)
 
-#4-----------------------------------------------------------
+# 4-----------------------------------------------------------
 from sklearn.linear_model import LogisticRegression
 
 X2 = df.drop(['target'], axis=1)
@@ -87,7 +92,7 @@ logreg_model = LogisticRegression(max_iter=1000)
 logreg_model.fit(X_train2, Y_train2)
 y_pred_test2 = logreg_model.predict(X_test2)
 
-#5-----------------------------------------------------------
+# 5-----------------------------------------------------------
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 accuracy = accuracy_score(Y_test2, y_pred_test2)
@@ -99,15 +104,8 @@ print("Confusion Matrix:")
 print(cm)
 print("-" * 100)
 
-#Lab3--------------------------------------------------------
-'''1. Использовать датасет, подготовленный на первой лабораторной работе
-2. Решить задачу регрессии для одного из непрерывных признаков в
-датасете. Оценить работу регрессионной модели
-3. Решить задачу классификации и оценить работу модели с помощью
-ROC-кривой
-4. Выгрузить результат работы на Github'''
-
-#2-----------------------------------------------------------
+# Lab3--------------------------------------------------------
+# 2-----------------------------------------------------------
 
 from sklearn.tree import DecisionTreeRegressor
 
@@ -120,34 +118,32 @@ mse_dt = mean_squared_error(Y_test, Y_pres_dt)
 rmse_dt = root_mean_squared_error(Y_test, Y_pres_dt)
 mae_dt = mean_absolute_error(Y_test, Y_pres_dt)
 print("Decision Tree Regression Results:")
-print("-"*100)
+print("-" * 100)
 print(f"MSE = {mse_dt}")
 print(f"RMSE = {rmse_dt}")
 print(f"MAE = {mae_dt}")
-print("-"*100)
+print("-" * 100)
 
-#3-----------------------------------------------------------
+# 3-----------------------------------------------------------
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import roc_curve, auc
+
 dt_classifier_model = DecisionTreeClassifier()
 dt_classifier_model.fit(X_train2, Y_train2)
 
 y_proba = dt_classifier_model.predict_proba(X_test2)
 
 fpr, tpr, thresholds = roc_curve(Y_test2, y_proba[:, 1])
-auc_metric=auc(fpr, tpr)
+auc_metric = auc(fpr, tpr)
 print("Decision Tree Classification Results:")
-print("-"*100)
+print("-" * 100)
 print(f"ROC AUC Score: {auc_metric:.4f}")
 print("-" * 100)
 plt.plot(fpr, tpr, marker='o')
-plt.ylim([0,1.1])
-plt.xlim([0,1.1])
+plt.ylim([0, 1.1])
+plt.xlim([0, 1.1])
 plt.ylabel('TPR')
 plt.xlabel('FPR')
 plt.title('ROC curve')
 plt.show()
-
-
-
